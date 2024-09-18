0185747d056ab48e2eb0384bbfc5527333b65a23
n0rmii <michal@zywer.pl>
2024-09-18 20:33:06 +0200

    fix: finish ternmem_init

--- ternmem.c
+++ ternmem.c
@@ -1,16 +1,17 @@
-#include <stdint.h>
 #include <malloc.h>
 #include "ternmem.h"
 #include "terntype.h"
 #include "ternops.h"
 
-static void* _ternmem_offset = NULL;
+static void* _ternmem_start = NULL;
+static void* _ternmem_end = NULL;
 
-int8_t ternmem_init(size_t memsize){	//Use console arguments to set program space
+int ternmem_init(size_t memsize){	//Use console arguments to set program space
 	if(memsize < MIN_TERN_MEMORY) return -1;
 	if(memsize > MAX_TERN_MEMORY) return -2;
 	void* check = calloc(memsize, sizeof(tword_t));
 	if(!check) return 1;
-
+	_ternmem_start = check;
+	_ternmem_end = check + memsize * sizeof(tword_t);
 	return 0;
 }
