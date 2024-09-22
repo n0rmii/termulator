aaa4edef4a398f0803ed6bc98a50b8ae8143fbb9
n0rmii <michal@zywer.pl>
2024-09-22 11:28:14 +0200

    fix: ternmem calloc blocksize from 8 to 9

--- ternmem.c
+++ ternmem.c
@@ -7,7 +7,7 @@ tword_t registers[13] = {0};
 
 void* ternmem_init(unsigned int memsize){	//Use console arguments to set program space
 	if(memsize < MIN_TERN_MEMORY || memsize > MAX_TERN_MEMORY) return NULL;
-	return calloc(memsize, sizeof(tword_t));
+	return calloc(memsize, sizeof(tword_t) + 1);
 }
 
 
